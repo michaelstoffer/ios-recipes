6660780cf116a1ab93eeb273fb62fecb1176878a
Michael Stoffer <mstoffer@michaelstoffer.com>
2019-05-22 21:14:00 -0400

    Setup storyboard and added view controllers

---
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Michael Stoffer on 5/22/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    // MARK: - @IBOutlets and Variables
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - @IBActions and Methods
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
